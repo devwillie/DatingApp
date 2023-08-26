4b7c58f1d223281e90e01d141c86742d01b81eff
devwillie <swillie100@gmail.com>
2023-08-26 15:33:57 -0700

    Updates from March, 2023

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,18 @@ export class AccountService {
 
   constructor(private http:HttpClient) { }
 
-  login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+  login(model: any) { 
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User): void => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user))
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
   }
 }
